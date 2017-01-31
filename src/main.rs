16a9aa96d268f8bb95d3c1c911d63d4f71ee93f3
Andrew 'Von Doom' Buntine <info@bunts.io>
2017-01-31 23:11:00 +1100

    Setup examples

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,27 @@ fn abs_all(input: &mut Cow<[i32]>) {
 }
 
 fn main() {
+    // No clone here because there is no mutation required.
     let s1 = [1,2,3];
     let mut i1 = Cow::from(&s1[..]);
-
-    let s2 = [1,2,3, -45, 5];
-    let mut i2 = Cow::from(&s2[..]);
-
-    // No clone here because there is no mutation required.
     abs_all(&mut i1);
-
-    // Here we clone because there is a mutation required.
-    abs_all(&mut i2);
 
     println!("IN: {:?}", s1);
     println!("OUT: {:?}", i1);
 
+
+    // Here we clone because there is a mutation required.
+    let s2 = [1,2,3, -45, 5];
+    let mut i2 = Cow::from(&s2[..]);
+    abs_all(&mut i2);
+
     println!("IN: {:?}", s2);
     println!("OUT: {:?}", i2);
+
+
+    // And no clone here because the data is already owned.
+    let mut v1 = Cow::from(vec![1,2,-3,4]);
+    abs_all(&mut v1);
+
+    println!("OUT: {:?}", v1);
 }
